12d23ed7a79a7546c5616416eeeef11e2d661685
Pieter Senden <pieter.senden@sweetmustard.be>
2024-09-16 14:16:00 +0200

    feat: extract request type and uri from method

--- src/main/kotlin/be/sweetmustard/springrestdocsgenerator/GenerateRestDocsTestAction.kt
+++ src/main/kotlin/be/sweetmustard/springrestdocsgenerator/GenerateRestDocsTestAction.kt
@@ -2,12 +2,68 @@ package be.sweetmustard.springrestdocsgenerator
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.psi.*
+import com.intellij.psi.util.PsiTypesUtil
+import com.intellij.psi.util.parentOfType
+import com.intellij.util.containers.stream
+import io.ktor.util.*
 
 
 class GenerateRestDocsTestAction : AnAction() {
     override fun actionPerformed(event: AnActionEvent) {
-        println("Plugin action performed")
+        val selectedMethod: PsiElement? = event.getData(CommonDataKeys.PSI_ELEMENT)
+
+        if (selectedMethod is PsiMethod) {
+            
+            val requestMappingOfMethod = getRequestMappingOfMethod(selectedMethod)
+
+            val requestMappingOfClass =
+                getRequestMappingOfParentalRestController(selectedMethod)
+            
+            val requestUri =
+                getUriFromAnnotation(requestMappingOfClass) + getUriFromAnnotation(requestMappingOfMethod)
+            
+            val requestMappingType =
+                requestMappingOfMethod.resolveAnnotationType()?.name?.removeSuffix("Mapping")
+            
+            var methodBody =
+                "mockMvc.perform(${requestMappingType?.toLowerCasePreservingASCIIRules()}(\"$requestUri\"))"
+            println(methodBody)
+        }
     }
-    
-    
+
+    private fun getRequestMappingOfParentalRestController(selectedMethod: PsiMethod): PsiAnnotation? {
+        val requestMappingClassLevel =
+            selectedMethod.parentOfType<PsiClass>()?.annotations
+                ?.filter {
+                    it.qualifiedName?.contains("org.springframework.web.bind.annotation")
+                        ?: false
+                }
+                ?.filter { it.qualifiedName?.contains("Mapping") ?: false }
+                ?.get(0)
+        return requestMappingClassLevel
+    }
+
+    private fun getRequestMappingOfMethod(method: PsiMethod): PsiAnnotation {
+        val requestMappingMethodLevel =
+            method.modifierList.annotations
+                .filter {
+                    it.qualifiedName?.contains("org.springframework.web.bind.annotation") ?: false
+                }
+                .filter {
+                    it.qualifiedName?.contains("Mapping") ?: false
+                }[0]
+        return requestMappingMethodLevel
+    }
+
+    private fun getUriFromAnnotation(requestMappingClassLevel: PsiAnnotation?): String? {
+        val uri =
+            requestMappingClassLevel?.parameterList?.attributes?.filter { it.name == null || it.name == "value" || it.name == "path" }
+                ?.getOrNull(0)
+        if (uri == null) {
+            return ""
+        }
+        return uri.literalValue
+    }
 }
