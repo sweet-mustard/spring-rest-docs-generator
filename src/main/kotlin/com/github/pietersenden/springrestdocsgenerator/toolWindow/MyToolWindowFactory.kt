f116739678cb2510c6949dc1ac155d23f6235a62
GitHub Action <action@github.com>
2024-09-16 11:00:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/pietersenden/springrestdocsgenerator/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/pietersenden/springrestdocsgenerator/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.pietersenden.springrestdocsgenerator.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.pietersenden.springrestdocsgenerator.MyBundle
+import com.github.pietersenden.springrestdocsgenerator.services.MyProjectService
 import javax.swing.JButton
 
 
