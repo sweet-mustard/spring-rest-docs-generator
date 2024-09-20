bba5277021bd6656e96b25929b1db99de186611e
Pieter Senden <pieter.senden@sweetmustard.be>
2024-09-20 15:33:12 +0200

    refactor: put methods in more logical order

--- src/main/kotlin/be/sweetmustard/springrestdocsgenerator/FieldDescription.kt
+++ src/main/kotlin/be/sweetmustard/springrestdocsgenerator/FieldDescription.kt
@@ -9,20 +9,6 @@ import com.intellij.psi.util.PsiTypesUtil
 import com.intellij.util.containers.stream
 import java.util.stream.Collectors
 
-val basicTypes = listOf("String", "Integer", "Boolean", "UUID", "Long", "Double").union(PsiTypes.primitiveTypes().stream().map { it.name }.toList()).plus(PsiTypes.voidType().name)
-
-data class FieldDescription(
-    val pathPrefix: String,
-    val path: String,
-    val description: String
-) {
-    fun depth() : Int {
-        return pathPrefix.count { it == '.'}
-    } 
-}
-
-data class TreeNode(val name: String, val subNodes: List<TreeNode>, val nodeType: NodeType)
-
 fun generateResponseFieldDescriptions(responseObjectType: PsiType): String {
     val responseFieldDescriptions = generateFieldDescriptions(responseObjectType, "")
     return buildFieldsDescriptionString(responseFieldDescriptions, HttpObjectType.RESPONSE)
@@ -70,6 +56,34 @@ fun generateFieldDescriptions(classType : PsiType, pathPrefix : String) : List<F
         }
     }
     return fieldDescriptions
+}
+
+fun buildFieldsDescriptionString(fieldDescriptions : List<FieldDescription>, httpObjectType: HttpObjectType): String {
+    val fieldDescriptionsGroupedByDepth = fieldDescriptions.stream()
+        .collect(Collectors.groupingBy { it.depth() })
+
+    val descriptions = ArrayList<String>()
+
+    for (entry in fieldDescriptionsGroupedByDepth) {
+        val fieldDescriptionsGroupedByPrefix = entry.value.stream()
+            .collect(Collectors.groupingBy { it.pathPrefix })
+
+        for (prefixGrouped in fieldDescriptionsGroupedByPrefix) {
+            val prefixLine = if (prefixGrouped.key.isNotEmpty()) ".andWithPrefix(\"" + prefixGrouped.key + "\", " else httpObjectType.fieldsDescription + "("
+            val fieldLines = prefixGrouped.value.stream()
+                .map { "fieldWithPath(\"" + it.path + "\").description(\"" + it.description + "\")" }
+                .reduce {s, t -> s + "," + System.lineSeparator() + t }
+                .orElse("")
+
+            val element =
+                prefixLine + System.lineSeparator() + fieldLines + ")"
+            descriptions.add(element)
+        }
+    }
+
+    return descriptions.stream()
+        .reduce {s, t, -> s + System.lineSeparator() + t}
+        .orElse("")
 }
 
 fun generateTree(field : PsiField) : List<TreeNode> {
@@ -141,40 +155,9 @@ fun buildJson(treeNode: TreeNode, indent : Int) : String {
 }
 
 fun buildJsonPiece(subNodes : List<TreeNode>, indent: Int) : String {
-    val jsonSubNodes = ArrayList<String>()
-    for (subNode in subNodes) {
-        jsonSubNodes.add(buildJson(subNode, indent + 1))
-    }
-    return jsonSubNodes.stream()
-        .reduce {a, b -> "$a ," + System.lineSeparator() + b}
-        .orElse("")
-}
-
-fun buildFieldsDescriptionString(fieldDescriptions : List<FieldDescription>, httpObjectType: HttpObjectType): String {
-    val fieldDescriptionsGroupedByDepth = fieldDescriptions.stream()
-        .collect(Collectors.groupingBy { it.depth() })
-
-    val descriptions = ArrayList<String>()
-
-    for (entry in fieldDescriptionsGroupedByDepth) {
-        val fieldDescriptionsGroupedByPrefix = entry.value.stream()
-            .collect(Collectors.groupingBy { it.pathPrefix })
-
-        for (prefixGrouped in fieldDescriptionsGroupedByPrefix) {
-            val prefixLine = if (prefixGrouped.key.isNotEmpty()) ".andWithPrefix(\"" + prefixGrouped.key + "\", " else httpObjectType.fieldsDescription + "("
-            val fieldLines = prefixGrouped.value.stream()
-                .map { "fieldWithPath(\"" + it.path + "\").description(\"" + it.description + "\")" }
-                .reduce {s, t -> s + "," + System.lineSeparator() + t }
-                .orElse("")
-
-            val element =
-                prefixLine + System.lineSeparator() + fieldLines + ")"
-            descriptions.add(element)
-        }
-    }
-
-    return descriptions.stream()
-        .reduce {s, t, -> s + System.lineSeparator() + t}
+    return subNodes.stream()
+        .map { subNode -> buildJson(subNode, indent + 1) }
+        .reduce { a, b -> "$a ," + System.lineSeparator() + b }
         .orElse("")
 }
 
@@ -182,6 +165,20 @@ private fun isListType(classType: PsiType) = classType.toString().contains("List
 
 private fun isBasicType(fieldType: PsiType) =
     basicTypes.stream().anyMatch { fieldType.toString().contains(it) }
+
+val basicTypes = listOf("String", "Integer", "Boolean", "UUID", "Long", "Double").union(PsiTypes.primitiveTypes().stream().map { it.name }.toList()).plus(PsiTypes.voidType().name)
+
+data class FieldDescription(
+    val pathPrefix: String,
+    val path: String,
+    val description: String
+) {
+    fun depth() : Int {
+        return pathPrefix.count { it == '.'}
+    }
+}
+
+data class TreeNode(val name: String, val subNodes: List<TreeNode>, val nodeType: NodeType)
 
 enum class HttpObjectType(val fieldsDescription: String) {
     RESPONSE("responseFields"),
